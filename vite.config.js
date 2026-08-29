da64d071a4313a6293b16f8b1dd4f8455f17e9c3
Swathi80-19 <sreeswathi418@gmail.com>
2026-08-29 20:20:06 +0530

    Update project

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/soa-react-app-4654"
+  base:"/soa-react-app-2400033213/"
 })
